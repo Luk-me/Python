d167013f1faeaa7384d1d0b04cbcd4515fa73ff1
Lusky-Git <1724880744@qq.com>
2020-01-13 10:48:30 +0800

    change github.py

--- API/github.py
+++ API/github.py
@@ -5,4 +5,14 @@ print(r.status_code)
 
 response_dict=r.json()
 
-print(response_dict.keys())
+# print(response_dict.keys())
+
+print("total respoistories:",response_dict['total_count'])
+
+repo_dicts=response_dict['items']
+print("Repositories returned:",len(repo_dicts))
+
+repo_dict = repo_dicts[0]
+print("\nKeys:",len(repo_dict))
+for key in sorted(repo_dict.keys()):
+    print(key)
